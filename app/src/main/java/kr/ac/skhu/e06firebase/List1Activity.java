ca217b3e61b2ea9c7360bb07876311206971ecfa
quack337 <quack337@gmail.com>
2017-03-10 18:56:56 +0900

    Update List1Activity.java

--- app/src/main/java/kr/ac/skhu/e06firebase/List1Activity.java
+++ app/src/main/java/kr/ac/skhu/e06firebase/List1Activity.java
@@ -14,7 +14,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
-
+//
 import java.util.ArrayList;
 import java.util.List;
 

c82bb2b99a10354b60a0654a65e62dfa9b1a417e
quack337 <quack337@gmail.com>
2017-03-10 18:40:33 +0900

    Update List1Activity.java

--- app/src/main/java/kr/ac/skhu/e06firebase/List1Activity.java
+++ app/src/main/java/kr/ac/skhu/e06firebase/List1Activity.java
@@ -46,7 +46,7 @@ public class List1Activity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError error) {
                 // 주석 추가1
-                Log.e("내태그", "서버 에러: ", error.toException());
+                Log.e("내태그", "파이어베이스 서버 에러: ", error.toException());
             }
         };
         myServerData01.addValueEventListener(listener1);
